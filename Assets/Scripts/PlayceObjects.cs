b63403ea07b28c74f7536c985073a29732a6615e
samurai <cpt.legion.337@gmail.com>
2024-04-18 19:49:20 +0300

    Исправление ошибки с появлением объекта, добаваление вращения объекта при создании

--- Assets/Scripts/PlayceObjects.cs
+++ Assets/Scripts/PlayceObjects.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class PlaceObjects : MonoBehaviour
 {
     public LayerMask layer;
+    public float rotateSpeed = 60f;
+    private void Start() {
+        PositionObject(); 
+    }
+    
     private void Update() {
+         PositionObject(); 
+        if(Input.GetMouseButtonDown(0))
+            Destroy(gameObject.GetComponent<PlaceObjects>());
+
+        if(Input.GetKey(KeyCode.LeftAlt))
+            transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+    private void PositionObject(){
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 1000f,layer ))
             transform.position = hit.point;
         
-        if(Input.GetMouseButtonDown(0))
-            Destroy(gameObject.GetComponent<PlaceObjects>());
+        
     }
 }
