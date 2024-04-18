f53a11c22e1718e3623323b0174af67629f3dc28
samurai <cpt.legion.337@gmail.com>
2024-04-18 19:32:01 +0300

    Добавление кнопок для создания и перемещения по карте дома и ангара

---
+++ Assets/Scripts/ButtunPlaceBuild.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtunPlaceBuild : MonoBehaviour
+{
+   public GameObject building;
+   public void PlaceBuild() {
+        Instantiate(building, Vector3.zero, Quaternion.identity);
+   }
+}
